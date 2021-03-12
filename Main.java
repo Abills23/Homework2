769b83ff563202ae6b5fb849607963cfcf8b00ea
Abills23 <abills23@icloud.com>
2021-03-12 00:25:23 -0500

    add new file

---
+++ Main.java
@@ -0,0 +1,8 @@
+package favartists;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kevin Gates, J. Cole, Lil Baby");
+    }
+}
+//C:\Users\abill\IdeaProjects\FavArtist\src\main\java\favartists\Main.java
